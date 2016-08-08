945d83d40743b9d3cfde13ce04dfd2a0e42fe6c1
Andrew Atwood <sandurz@gmail.com>
2016-08-07 20:21:45 -0500

    Prototyping Domoticz prototype

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,42 @@
+function Domoticz(server){
+    this.server = server;
+    this.rooms = {};
+}
+
+Domoticz.prototype.init = function(){
+    return this.getRooms();
+}
+
+Domoticz.prototype.getRooms = function(){
+    var deferred = $.Deferred();
+    var url = this.server + '/json.htm?type=plans&order=name&used=true';
+    var rooms = {};
+    $.get(
+        url,
+        function(data){
+            if(data.status != 'OK'){
+                deferred.reject();
+                return null;
+            }
+            for( var i = 0; i < data.result.length; i++){
+                var result = data.result[i];
+                rooms[result.idx] = result.Name;
+            }
+            console.log(rooms);
+            this.rooms = rooms;
+            console.log(this.rooms);
+            deferred.resolve();
+            return true;
+        }
+    )
+    return deferred;
+}
+
+var domoticz = new Domoticz('http://localhost:8080');
+domoticz.init().then(function(){
+    console.log(domoticz);
+});
+
 new Vue({
     el: '#app',
     data: {
