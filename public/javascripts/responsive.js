f006c0b9ea698d6d7389909c0436cab89cc8f9c1
Andrew Atwood <sandurz@gmail.com>
2016-08-09 08:12:12 -0500

    Smart sizing for scenes container

--- public/javascripts/responsive.js
+++ public/javascripts/responsive.js
@@ -1,10 +1,17 @@
 function size(){
+    //Set scene width
     var square = accessories.width();
     var gutter = accessories.outerWidth(true) - square;
     console.log('Resizing - square: %s gutter: %s',square,gutter);
     var longWidth = square*2 + gutter;
     scenes.width(longWidth).css('margin-right',gutter/2,'margin-left',gutter/2);
-    
+
+    //Set scene wrapper width
+    $('.scenes').each(function(){
+        var columnCount = Math.ceil($(this).children('.action.scene').length/2)
+        var totalWidth = 20 + columnCount*longWidth + 55;
+        $(this).width(totalWidth);
+    });
 }
 
 function bind(){
